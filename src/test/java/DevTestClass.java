89033b441f376381809a2e9119d901301a30a4b7
prakash-rohan <105876122+prakash-rohan@users.noreply.github.com>
2022-05-19 22:27:42 +0530

    Just adding one chillPill statement in test
    Adding a bro code in test class

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -1,5 +1,7 @@
 public class DevTestClass {
     public static void main(String[] args) {
         System.out.println("Hello From Dev Class");
+        
+        System.out.println("Please do the same shit here too, Maza aata hai");
     }
 }
